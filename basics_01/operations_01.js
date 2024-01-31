2712414a584439686a4f234279e445a8c4cb345e
Ankita Saha <65508136+an861@users.noreply.github.com>
2024-01-31 07:22:55 +0000

    prefix_postfix operations

---
+++ basics_01/operations_01.js
@@ -0,0 +1,79 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+console.log(typeof negvalue)
+
+// ARITHMETIC OPERATIONS
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**4)
+console.log(2/2)
+console.log(2%2)
+
+
+// let test = 1 + "2"
+// let test = "2" + 1
+// let test = "2" + 1 + 2
+// let test = 2 + 1 + "2"
+let test = 2 + "3" + 2
+console.log(test)
+console.log(typeof test)
+
+console.log(+true)
+console.log(-true)
+console.log(+false)
+console.log(-false)
+console.log(+"")
+console.log(-"")
+// console.log(true+)  // SyntaxError
+
+// console.log(3 + 4 % 7 ** 5) // Not a good practise
+
+// let num1 = num2 = num3 = 2+2 // Not a good practise
+
+
+/* 
+
+***************************PREFIX POSTFIX*****************************************
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+ */
+
+let counter = 100
+// counter++    // 101
+// ++counter    // 101
+// counter --   // 99
+// --counter    // 99
+console.log(counter);
+
+
+// let a = 10
+// const b = a++
+// console.log(`a : ${a}, b : ${b}`)  // a = 11, b = 10
+
+
+// let a = 10
+// const b = ++a
+// console.log(`a : ${a}, b : ${b}`)  // a = 11, b = 11
+
+
+// let a = 10
+// const b = a--
+// console.log(`a : ${a}, b : ${b}`)  // a = 9, b = 10
+
+
+// let a = 10
+// const b = --a
+// console.log(`a : ${a}, b : ${b}`)  // a = 9, b = 9
+
+
+// let a = 3n                      // 3n is a bigInteger
+// const b = a++
+// console.log(`a:${a}, b:${b}`);  //a = 4, b = 3
+
+
+
+
+
